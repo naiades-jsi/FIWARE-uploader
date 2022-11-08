e8c6b37541ded7d167dc1310869279c22f3518ad
Klemen Kenda <klemen.kenda@ijs.si>
2022-11-08 15:06:57 +0100

    Fixing from time

--- post/post_FIWARE.py
+++ post/post_FIWARE.py
@@ -239,8 +239,11 @@ class SendData():
             from_time = timestamp_in_ns/1000000000
             to_time = from_time + horizon_in_h * 3600
             # if we have daily horizon, then from time is 24 hours before to_time
+            # else we are using from_time that is 1 hour before to_time
             if (horizon_in_h >= 24):
                 from_time = to_time - 24 * 3600
+            else:
+                from_time = to_time - 1 * 3600
 
             # Cast time in seconds
             prediction_time_timestamp = datetime.utcfromtimestamp(int(prediction_time))
